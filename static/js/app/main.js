f7212e2fbe8c8de6d866c38f3e940e53dc1f823e
shadvls <yanshaaa28@gmail.com>
2013-04-15 12:00:00 +0000

    feat: add Backbone Model/Collection with task CRUD wiring
    Create Task model with toggle method and TaskList collection with
    byStatus/pending/done filtering and progress calculation.

---
+++ static/js/app/main.js
@@ -0,0 +1,10 @@
+;(function ($, _, Backbone, Handlebars) {
+
+  var App = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Router: {}
+  };
+
+})(jQuery, _, Backbone, Handlebars);
---
+++ static/js/app/models/task.js
@@ -0,0 +1,46 @@
+var Task = Backbone.Model.extend({
+  urlRoot: '/api/tasks',
+
+  defaults: {
+    title: '',
+    description: '',
+    status: 'pending',
+    priority: 0,
+    category: 'general'
+  },
+
+  toggle: function () {
+    this.save({ status: this.get('status') === 'done' ? 'pending' : 'done' }, { patch: true });
+  }
+});
+
+var TaskList = Backbone.Collection.extend({
+  url: '/api/tasks',
+  model: Task,
+
+  comparator: function (task) {
+    var priority = task.get('priority');
+    var created = new Date(task.get('created_at')).getTime();
+    return -(priority * 10000000000000 + created);
+  },
+
+  byStatus: function (status) {
+    return this.filter(function (task) {
+      return task.get('status') === status;
+    });
+  },
+
+  pending: function () {
+    return this.byStatus('pending');
+  },
+
+  done: function () {
+    return this.byStatus('done');
+  },
+
+  progress: function () {
+    var total = this.length;
+    if (total === 0) return 100;
+    return Math.round((this.done().length / total) * 100);
+  }
+});
