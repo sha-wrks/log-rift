a01b7118b69dc2eb4c54982f78b61a225b00d0d4
shadvls <yanshaaa28@gmail.com>
2013-04-19 12:00:00 +0000

    feat: wire up Backbone app bootstrap
    Initialize task collection, router, and history on DOM ready.
    Include all JS files in proper load order.

--- static/js/app/main.js
+++ static/js/app/main.js
@@ -7,4 +7,17 @@
     Router: {}
   };
 
+  window.App = App;
+
+  $(function () {
+
+    App.tasks = new App.Collections.TaskList();
+
+    App.router = new App.Router();
+
+    Backbone.history.start();
+
+    App.router.navigate('', { trigger: true });
+  });
+
 })(jQuery, _, Backbone, Handlebars);
