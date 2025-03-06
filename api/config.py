52a317712f11bc0549bcee58ce39b5d3789f86da
shadvls <yanshaaa28@gmail.com>
2025-03-06 12:00:00 +0000

    feat: add centralized API configuration module

---
+++ api/config.py
@@ -0,0 +1,5 @@
+import os
+class Config:
+    DATA_DIR = os.environ.get("ORDO_DATA_DIR", "/tmp/ordo-data")
+    DEBUG = os.environ.get("FLASK_ENV") == "development"
+    PORT = int(os.environ.get("PORT", 5000))
